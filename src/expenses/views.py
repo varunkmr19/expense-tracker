02bd41ecca0a6a8483f70258b01e7e17572f6db6
varunkmr19 <varun-kumar@live.com>
2021-02-28 19:42:27 +0530

    add decorators

--- src/expenses/views.py
+++ src/expenses/views.py
@@ -1,10 +1,10 @@
-import re
-from django import contrib
 from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib import messages
 from django.urls import reverse_lazy
 from django.views.generic.edit import DeleteView
+from django.contrib.messages.views import SuccessMessageMixin
 from .models import Category, Expense
 from . import utils
 
@@ -20,6 +20,7 @@ def index_view(request):
     return render(request, 'expenses/index.html', context)
 
 
+@login_required(login_url='/auth/login')
 def add_expenses(request):
     categories = Category.objects.all()
     context = {
@@ -55,6 +56,7 @@ def add_expenses(request):
     return render(request, 'expenses/add_expenses.html', context)
 
 
+@login_required(login_url='/auth/login')
 def edit_expense(request, expense_id):
     category = Category.objects.all()
     expense = Expense.objects.get(pk=expense_id)
@@ -94,7 +96,9 @@ def edit_expense(request, expense_id):
     return render(request, 'expenses/edit_expense.html', context)
 
 
-class DeleteExpense(DeleteView):
+class DeleteExpense(SuccessMessageMixin, LoginRequiredMixin, DeleteView):
     model = Expense
     success_url = reverse_lazy('expenses')
+    login_url = '/auth/login'
+    success_message = 'Expense deleted successfully'
     template_name = 'expenses/delete_expense.html'
