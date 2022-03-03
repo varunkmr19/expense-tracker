8d7462a92ba6f76edf38e9eb4283e8c3550c4bc9
Varun Kumar <varun-kumar@live.com>
2022-03-03 22:47:55 +0530

    add get and post method for transactions

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from api import views
 
 urlpatterns = [
-  path('categories/', views.ListCategories.as_view())
+  path('categories/', views.ListCategories.as_view()),
+  path('transactions/', views.ListTransactions.as_view())
 ]
--- api/views.py
+++ api/views.py
@@ -1,8 +1,10 @@
+from unicodedata import category
+from urllib import request
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from expense_app.models import Category
-from api.serializers import CategorySerializer
+from expense_app.models import Category, SubCategory, Transaction
+from api.serializers import CategorySerializer, TransactionSerializer
 
 
 class ListCategories(APIView):
@@ -16,3 +18,31 @@ class ListCategories(APIView):
     categories = Category.objects.prefetch_related('subcategory').all()
     serializer = CategorySerializer(categories, many=True)
     return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class ListTransactions(APIView):
+  """
+  View to list all transactions
+  """
+  def get(self, request, format=None):
+    """
+    Return a list of all transactions made by the user
+    """
+    transactions = Transaction.objects.filter(user=request.user)
+    serializer = TransactionSerializer(transactions, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+  def post(self, request, format=None):
+    """
+    Save a transaction against the logged in user
+    """
+    data = request.data
+    try:
+      sub_category = SubCategory.objects.get(pk=data.get('category'))
+      data.pop('category')
+      transaction = Transaction.objects.create(user=request.user, category=sub_category, **data)
+      serializer = TransactionSerializer(transaction)
+      return Response(serializer.data, status=status.HTTP_201_CREATED)
+    except SubCategory.DoesNotExist:
+      return Response(exception=True, status=status.HTTP_404_NOT_FOUND)
+    
