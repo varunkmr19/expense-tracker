355066a0c9ac634fe7ca599bcbb04bc1c23c2826
Varun Kumar <varun-kumar@live.com>
2022-03-05 06:48:02 +0530

    api for retrive, update and delete transaction

--- api/urls.py
+++ api/urls.py
@@ -3,5 +3,6 @@ from api import views
 
 urlpatterns = [
   path('categories/', views.ListCategories.as_view()),
-  path('transactions/', views.ListTransactions.as_view())
+  path('transactions/', views.ListTransactions.as_view()),
+  path('transactions/<int:pk>', views.TransactionDetail.as_view()),
 ]
--- api/views.py
+++ api/views.py
@@ -1,8 +1,9 @@
+from unicodedata import category
 from django.http import Http404
 from rest_framework import status
 from rest_framework.views import APIView
 from expense_app.models import Category, SubCategory, Transaction
-from api.serializers import CategorySerializer, TransactionSerializer
+from api.serializers import CategorySerializer, SubCategorySerializer, TransactionSerializer
 from api.response import response
 
 
@@ -45,3 +46,42 @@ class ListTransactions(APIView):
     except SubCategory.DoesNotExist:
       return response(status=status.HTTP_404_NOT_FOUND, message="Category not found")
     
+
+class TransactionDetail(APIView):
+  """
+  Retrieve, update or delete a transaction instance.
+  """
+  def get_object(self, pk):
+    try:
+      return Transaction.objects.get(pk=pk)
+    except Transaction.DoesNotExist:
+      return response(status=status.HTTP_404_NOT_FOUND, message="Transaction not found")
+  
+  def get(self, request, pk, format=None):
+    transaction = self.get_object(pk)
+    serializer = TransactionSerializer(transaction)
+    return response(status=status.HTTP_200_OK, message='success', data=serializer.data)
+
+  def put(self, request, pk, format=None):
+    data = request.data
+    transaction = self.get_object(pk)
+    try:
+      category = SubCategory.objects.get(pk=data.get('category'))
+      transaction = Transaction.objects.get(pk=data.get('id'))
+
+      transaction.amount = data.get('amount')
+      transaction.description = data.get('description')
+      transaction.category = category
+      transaction.is_credit = data.get('is_credit')
+      transaction.save()
+      serializer = TransactionSerializer(transaction)
+    except SubCategory.DoesNotExist:
+      return response(status=status.HTTP_400_BAD_REQUEST, message="Category not found: please check the id")
+    except Transaction.DoesNotExist:
+      return response(status=status.HTTP_400_BAD_REQUEST, message="Transaction not found: please check the id")
+    return response(status=status.HTTP_200_OK, message="updated successfully", data=serializer.data)
+
+  def delete(self, request, pk, format=None):
+    transaction = self.get_object(pk)
+    transaction.delete()
+    return response(status=status.HTTP_204_NO_CONTENT, message='deleted successfully')
