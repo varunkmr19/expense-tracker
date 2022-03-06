53ba938290497dc15242ad280c448ef9adb8e84e
Varun Kumar <varun-kumar@live.com>
2022-03-06 14:19:56 +0530

    add permissions

--- api/views.py
+++ api/views.py
@@ -1,8 +1,21 @@
-from rest_framework import status
+from curses import reset_shell_mode
+from django.http import Http404
+from django.core.exceptions import PermissionDenied
+from rest_framework import status, permissions
 from rest_framework.views import APIView
 from expense_app.models import Category, SubCategory, Transaction
 from api.serializers import CategorySerializer, TransactionSerializer, UserSerializer
 from api.response import response
+
+
+class IsOwner(permissions.BasePermission):
+  """
+  Custom permission to only allow owners of an object to perform CRUD operations on it.
+  """
+  def has_permission(self, request, view):
+      return request.user and request.user.is_authenticated
+  def has_object_permission(self, request, view, obj):
+      return obj.user == request.user
 
 
 class UserRegistration(APIView):
@@ -33,6 +46,7 @@ class ListTransactions(APIView):
   """
   View to list all transactions
   """
+  permission_classes = (IsOwner,)
   def get(self, request, format=None):
     """
     Return a list of all transactions made by the user
@@ -60,20 +74,25 @@ class TransactionDetail(APIView):
   """
   Retrieve, update or delete a transaction instance.
   """
-  def get_object(self, pk):
+  permission_classes = (permissions.IsAuthenticated, IsOwner,)
+  def get_object(self, pk, user):
     try:
-      return Transaction.objects.get(pk=pk)
+      instance = Transaction.objects.get(pk=pk)
+      if instance.user == user:
+        return instance
+      raise PermissionDenied
     except Transaction.DoesNotExist:
-      return response(status=status.HTTP_404_NOT_FOUND, message="Transaction not found")
+      raise Http404
+
   
   def get(self, request, pk, format=None):
-    transaction = self.get_object(pk)
+    transaction = self.get_object(pk, request.user)
     serializer = TransactionSerializer(transaction)
     return response(status=status.HTTP_200_OK, message='success', data=serializer.data)
 
   def put(self, request, pk, format=None):
     data = request.data
-    transaction = self.get_object(pk)
+    transaction = self.get_object(pk, request.user)
     try:
       category = SubCategory.objects.get(pk=data.get('category'))
       transaction = Transaction.objects.get(pk=data.get('id'))
@@ -91,6 +110,6 @@ class TransactionDetail(APIView):
     return response(status=status.HTTP_200_OK, message="updated successfully", data=serializer.data)
 
   def delete(self, request, pk, format=None):
-    transaction = self.get_object(pk)
+    transaction = self.get_object(pk, request.user)
     transaction.delete()
     return response(status=status.HTTP_204_NO_CONTENT, message='deleted successfully')
